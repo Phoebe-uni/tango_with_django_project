ca224489145fc0ca31a458b87c87b1079ae885c7
Yu <2572252y@student.gla.ac.uk>
2021-02-11 03:37:20 +0800

    ch.9&10 commit

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -95,6 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6,}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -104,6 +105,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -129,3 +134,5 @@ MEDIA_URL = '/media/'
 # Path to the single location to store our project’s
 # static files
 STATICFILES_DIRS = [STATIC_DIR, ]
+
+LOGIN_URL = 'rango:login'
